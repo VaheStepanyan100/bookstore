0e320728e00a9716e1ea92b3875f21282e0393ad
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-15 19:48:16 +0400

    Delete App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
