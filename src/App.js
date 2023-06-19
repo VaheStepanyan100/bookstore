f6bd365ccdfd79f17db17382d0a10d02877b1145
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-20 00:53:50 +0400

    Wrap <App> with the <Provider> component from react-redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
+import store from './redux/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
-    </>
+    </Provider>
   );
 }
 
