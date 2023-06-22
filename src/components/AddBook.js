0329e95cb993b86d051994d6987f3875dee4e45f
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-22 19:38:39 +0400

    Style Add Book form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { postBook, getBooks } from '../redux/books/booksSlice';
 
@@ -21,7 +22,7 @@ const AddBook = () => {
   };
 
   return (
-    <section>
+    <Section>
       <h1 className="title">Add Book</h1>
       <form className="form">
         <input
@@ -42,8 +43,52 @@ const AddBook = () => {
           Add Book
         </button>
       </form>
-    </section>
+    </Section>
   );
 };
 
 export default AddBook;
+
+const Section = styled.section`
+  border-top: 1px solid #e8e8e8;
+  margin: 2rem 6.25rem;
+  .title {
+    height: 1.5rem;
+    margin: 1.813rem 32.75rem 1.188rem 0;
+    font-size: 1.25rem;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.18px;
+    color: #888;
+  }
+  .form {
+    display: flex;
+    justify-content: space-between;
+
+    .book-title {
+      height: 2.813rem;
+      border-radius: 4px;
+      border: solid 1px var(--white);
+      background-color: #fff;
+      padding-left: 1rem;
+      width: 55.15%;
+    }
+    .book-author {
+      height: 2.813rem;
+      border-radius: 4px;
+      border: solid 1px var(--white);
+      background-color: #fff;
+      padding-left: 1rem;
+      width: 23.67%;
+    }
+    .add-book {
+      height: 2.813rem;
+      border-radius: 3px;
+      color: #fff;
+      background-color: #2e90f0;
+      width: 15.28%;
+    }
+  }
+`;
